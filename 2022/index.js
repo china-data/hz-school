dfbadb1e8c3515a88a958681ecebba3c9df3a1f6
zhoukekestar <zhoukekestar@163.com>
2022-08-07 17:40:59 +0800

    add

--- 2022/index.js
+++ 2022/index.js
@@ -40,14 +40,41 @@ fs.writeFileSync('./schoolLocation.json', JSON.stringify(locations));
 !(async () => {
 
   for (let i = 0; i < locations.length; i++) {
+    // 学校详细信息
     const code = locations[i].code;
     const fname = `./detail/${code}.json`;
+
+    // 学校详细信息缓存
     if (fs.existsSync(fname) === false) {
       console.log(`fetch ${code} ing...`);
       const res = await fetch(`https://rxyj.hzedu.gov.cn/hzjyAppServer/api/AppSchoolInfo/2022/${locations[i].code}`)
-        .then(d => d.json())
-
+      .then(d => d.json())
+      
       fs.writeFileSync(fname, JSON.stringify(res));
     }
+    
+
+    
   }
+
+
+  // 学区信息查询
+  const schoolDistrict = {};
+
+  for (let i = 0; i < locations.length; i++) {
+    const code = locations[i].code;
+    const fname = `./detail/${code}.json`;
+    const json = JSON.parse(fs.readFileSync(fname));
+    const sdlist = json.result.appSchoolDistrictInfoEntityList;
+
+    schoolDistrict[code] = [];
+
+    for(let j = 0; j < sdlist.length; j++) {
+      schoolDistrict[code].push({
+        name: sdlist[j].name,
+      })  
+    }
+  }
+
+  fs.writeFileSync('./schoolDistrict.json', JSON.stringify(schoolDistrict));
 })();
